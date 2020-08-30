2d90e7badac0205ca33e06d0e874bb7cfbbbf1f4
Mikail Khan <mikail.khan45@gmail.com>
2020-08-30 15:59:24 -0400

    started scanner

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,69 @@
 use std::error::Error;
 use std::io::{self, Read};
 
+#[allow(dead_code)]
+#[derive(Debug)]
+enum TokenType {
+    LParen,
+    RParen,
+    LBrace,
+    RBrace,
+    Comma,
+    Dot,
+    Plus,
+    Minus,
+    Slash,
+    Star,
+    Bang,
+    BangEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+    Identifier,
+    Str,
+    Number,
+    And,
+    Or,
+    Struct,
+    If,
+    Else,
+    Elif,
+    True,
+    False,
+    Function,
+    For,
+    While,
+    Print,
+    EOF,
+}
+
+pub trait Literal: std::fmt::Display { }
+
+struct Token {
+    pub ty: TokenType,
+    pub lexeme: String,
+    pub literal: Box<dyn Literal>,
+    pub line: usize,
+}
+
+impl Token {
+    pub fn new(ty: TokenType, lexeme: String, literal: Box<dyn Literal>, line: usize) -> Self {
+        Token {
+            ty,
+            lexeme,
+            literal,
+            line,
+        }
+    }
+}
+
+impl std::fmt::Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?} {} {}", self.ty, self.lexeme, self.literal)
+    }
+}
+
 fn run(code: &String) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
